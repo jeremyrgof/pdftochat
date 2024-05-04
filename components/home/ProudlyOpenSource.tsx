e78cbfb174ee35aa7022a3cb699b1adada326a8b
Jeremy Gofman <jeremy@jeremys-mbp.mynetworksettings.com>
2024-05-04 15:50:56 -0700

    jeremy testing

--- components/home/ProudlyOpenSource.tsx
+++ components/home/ProudlyOpenSource.tsx
@@ -4,7 +4,7 @@ const ProudlyOpenSource = () => {
   return (
     <div className="container pt-[55px] mx-auto pb-[53px] sm:pb-[174px] px-8 sm:px-0">
       <h2 className="text-center text-3xl sm:text-[60px] font-normal leading-[72px] tracking-[-0.6px] sm:tracking-[-1.2px] bg-clip-text text_bg pb-3 sm:pb-[30px]">
-        Proudly open-source
+        Proudly open-source (Jeremy Testing app)
       </h2>
       <p className="text-primary  text-center text-[17px] sm:text-3xl font-light sm:leading-[34.5px] sm:tracking-[-0.6px] leading-[22px] tracking-[-0.34px] max-w-[728px] mx-auto pb-[18px] sm:pb-11">
         Our source code is available on GitHub - feel free to read, review, or
